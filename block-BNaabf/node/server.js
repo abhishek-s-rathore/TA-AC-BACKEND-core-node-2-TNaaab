caf988bfd23602df9ef8ae2ff1ef46a574fbf6ca
Abhishek <abhishek.990914@gmail.com>
2021-08-09 11:43:23 +0530

    block Refectored

--- block-BNaabf/node/server.js
+++ block-BNaabf/node/server.js
@@ -6,16 +6,37 @@ const path = require('path');
 
 // Path
 // 1
-console.log(`../client/index.js`);
+// console.log(`../client/index.js`);
 // 2
-// console.log( path.join(__dirname));
+// console.log(path.join(__dirname, '..', 'client/index.js'));
 
 // Server
 
 let server = http.createServer(handleRequest);
 
-function handleRequest(req, res) {}
+function handleRequest(req, res) {
+  let store = ``;
+  req.on('data', (chunk) => {
+    store += chunk;
+  });
 
-server.listen(1212, 'localhost', () => {
+  req.on('end', () => {
+    if (req.method === `GET` && req.url === '/form') {
+      res.setHeader('Content-Type', 'text/html');
+      fs.createReadStream('./form.html').pipe(res);
+    }
+
+    if (req.method === `POST` && req.url === '/form') {
+      var parsedData = JSON.parse(JSON.stringify(querystring.parse(store)));
+      console.log(parsedData);
+      res.setHeader('Content-Type', 'text/html');
+      res.end(
+        `<h2>${parsedData.name}</h2><h3>${parsedData.email}</h3><h4>${parsedData.age}</h4>`
+      );
+    }
+  });
+}
+
+server.listen(1232, 'localhost', () => {
   console.log('Server listning to port 1212!');
 });
